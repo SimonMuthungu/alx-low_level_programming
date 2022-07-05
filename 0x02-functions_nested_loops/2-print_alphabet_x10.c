ac360a5ffda5f53d2dd5946fa1c55ab13dc037ab
SimonMuthungu <henrymsechu1@gmail.com>
2022-07-05 22:00:01 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,14 +10,15 @@ void print_alphabet_x10(void)
 {
 
 int count = 10;
+int i;
 
 while (count > 0)
 {
 for (i = 'a'; i <= 'z'; i++)
 {
-_printchar(i);
+_putchar(i);
 count = count - 1;
 }
-_printchar('\n');
+_putchar('\n');
 }
 }
