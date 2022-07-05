25b25ce96ef39923dedbdfe95efb96ccb23f67af
SimonMuthungu <henrymsechu1@gmail.com>
2022-07-05 22:36:04 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,25 @@
+#include "main.h"
 
+/**
+*_isalpha() checks for alpha
+*
+*Return: 0 (Always success)
+*/
+
+void _isalpha(int c)
+{
+char i,d;
+for (i = 'a'; i <= 'z'; i++)
+{
+  for (d = 'A'; d <= 'Z'; d++)
+  {
+  if (c == i || c == d)
+  {
+    return (1);
+  else
+  {
+    return (0);
+  }
+  }
+  }
+}
