1d514b0a6f1f9c15d09b85a6de16e061112ec3f5
SimonMuthungu <henrymsechu1@gmail.com>
2022-07-05 22:55:07 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,17 +3,18 @@
 /**
 *_isalpha() checks for alpha
 *
+*@c: int type
 *Return: 0 (Always success)
 */
 
 int _isalpha(int c)
 {
-  if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
-  {
-    return (1);
-  }
-  else
-  {
-    return (0);
-  }
+if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
