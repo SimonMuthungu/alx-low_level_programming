d055e16ec2868e484e233ef8e52ca3bb8500b387
SimonMuthungu <henrymsechu1@gmail.com>
2022-07-08 21:40:59 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,14 @@
+#include "main.h"
+
 /**
 *print_numbers: prints the nine numbers
 *@i: int i
 */
 
-void print_numbers(void)
+int print_numbers(void)
 {
   int i;
-  for (i = 0;i <=9; i++)
+  for (i = 0;i <= 9; i++)
   {
     _putchar(i);
   }
