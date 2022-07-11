5e62e5530cd9ca6b3e55fe449b78f8e9c97301ff
SimonMuthungu <henrymsechu1@gmail.com>
2022-07-12 01:22:25 +0300

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.io"
+/**
+ * main - updates value to 98
+ *
+ * return: 0 (always success)
+ */
+
+int main(int *n)
+{
+	*n = 98;
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Readme with app, thats great
