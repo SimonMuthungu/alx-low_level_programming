eb24e4f4c5c6d5205640ffb84e67b0b3bd969630
SimonMuthungu <henrymsechu1@gmail.com>
2022-07-13 11:24:53 +0300

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,1 @@
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-int _putchar(char s);
-void puts2(char *str);
-void print_array(int *a, int n);
+char *_strncat(char *dest, char *src, int n);
