75445399da659d421b442dc33f2e6d9e94e9f382
SimonMuthungu <henrymsechu1@gmail.com>
2022-07-13 11:23:44 +0300

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  _strcat - concats two strs
+ *
+ *   @dest: char one
+ *   @src: char two
+ *
+ *   Return: newdest
+ */
+
+int _strcat(char *dest, char *src)
+{
+	char *newdest;
+
+	newdest = dest, src;
+
+	return (*newdest);
+}
