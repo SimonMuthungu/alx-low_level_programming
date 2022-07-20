143c71b632cb94a79c0293f7f5e5cedf8894e99f
SimonMuthungu <henrymsechu1@gmail.com>
2022-07-21 00:12:29 +0300

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,15 +2,16 @@
 /**
  * _puts_recursion - a recursion method
  * @s: the char to be printed
+ * return (1): not success
  */
-int _puts_recursion(char *s)
+void _puts_recursion(char *s)
 {
 	int i = 0;
 
 	_putchar(s[i]);
 
 	{
-		return 1;
+		return (1);
 	}
-	_puts_recursion(s[i+1]);
+	_puts_recursion(s[i + 1]);
 }
